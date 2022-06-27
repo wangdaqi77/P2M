bc4bc2818df402eca6209579ff076ba30c482500
wangqi <wangqi@okay.cn>
2022-06-27 19:30:05 +0800

    新增：P2M.launcherOf()、P2M.serviceOf()、P2M.eventOf() 新增语法糖：ModuleClass.api、ModuleClass.launcher、ModuleClass.service、ModuleClass.event

--- example/module-main/src/main/java/com/p2m/example/main/pre_api/MainActivity.kt
+++ example/module-main/src/main/java/com/p2m/example/main/pre_api/MainActivity.kt
@@ -9,6 +9,8 @@ import android.widget.Toast
 import androidx.lifecycle.Observer
 import com.p2m.core.P2M
 import com.p2m.annotation.module.api.ApiLauncher
+import com.p2m.core.api
+import com.p2m.core.launcher
 import com.p2m.example.main.R
 import com.p2m.example.account.p2m.api.Account
 import com.p2m.example.main.p2m.api.Main
@@ -74,10 +76,12 @@ class MainActivity : AppCompatActivity() {
 
         // 停止服务
         findViewById<Button>(R.id.main_btn_stop_service).setOnClickListener {
-            P2M.apiOf(Account::class.java)
-                .launcher
-                .serviceOfTest
-                .stop(this)
+            Account::class.launcher.serviceOfTest.stop(this)
+//            等同于
+//            P2M.apiOf(Account::class.java)
+//                .launcher
+//                .serviceOfTest
+//                .stop(this)
         }
 
         // 退出登录
--- p2m-core/src/main/java/com/p2m/core/P2M.kt
+++ p2m-core/src/main/java/com/p2m/core/P2M.kt
@@ -3,12 +3,15 @@ package com.p2m.core
 import android.content.Context
 import android.os.Looper
 import androidx.annotation.MainThread
+import com.p2m.annotation.module.api.ApiEvent
 import com.p2m.core.config.P2MConfigManager
 import com.p2m.core.internal._P2M
 import com.p2m.core.launcher.GlobalLaunchActivityInterceptor
 import com.p2m.core.launcher.LaunchActivityChannel
 import com.p2m.annotation.module.api.ApiLauncher
+import com.p2m.annotation.module.api.ApiService
 import com.p2m.core.module.*
+import kotlin.reflect.KClass
 
 object P2M : ModuleApiProvider {
 
@@ -49,6 +52,51 @@ object P2M : ModuleApiProvider {
      */
     override fun <MODULE_API : ModuleApi<*, *, *>> apiOf(clazz: Class<out Module<MODULE_API>>): MODULE_API {
         return _P2M.apiOf(clazz)
+    }
+
+    /**
+     * Get `launcher` for a module.
+     *
+     * @param clazz its class name is defined module name in `settings.gradle`.
+     * @return a singleton instance of `launcher`.
+     *
+     * @see Module
+     * @see ModuleApi
+     * @see ModuleLauncher
+     * @see ApiLauncher
+     */
+    fun <LAUNCHER : ModuleLauncher, MODULE_API : ModuleApi<LAUNCHER, *, *>> launcherOf(clazz: Class<out Module<MODULE_API>>): LAUNCHER {
+        return _P2M.apiOf(clazz).launcher
+    }
+
+    /**
+     * Get `service` for a module.
+     *
+     * @param clazz its class name is defined module name in `settings.gradle`.
+     * @return a singleton instance of `service`.
+     *
+     * @see Module
+     * @see ModuleApi
+     * @see ModuleService
+     * @see ApiService
+     */
+    fun <SERVICE : ModuleService, MODULE_API : ModuleApi<*, SERVICE, *>> serviceOf(clazz: Class<out Module<MODULE_API>>): SERVICE {
+        return _P2M.apiOf(clazz).service
+    }
+
+    /**
+     * Get `event` for a module.
+     *
+     * @param clazz its class name is defined module name in `settings.gradle`.
+     * @return a singleton instance of `event`.
+     *
+     * @see Module
+     * @see ModuleApi
+     * @see ModuleEvent
+     * @see ApiEvent
+     */
+    fun <EVENT : ModuleEvent, MODULE_API : ModuleApi<*, *, EVENT>> eventOf(clazz: Class<out Module<MODULE_API>>): EVENT {
+        return _P2M.apiOf(clazz).event
     }
 
     /**
@@ -98,3 +146,27 @@ object P2M : ModuleApiProvider {
         _P2M.interceptorContainer.unregisterGlobal(launchActivityInterceptor)
     }
 }
+
+inline val <MODULE_API : ModuleApi<*, *, *>> Class<out Module<MODULE_API>>.api: MODULE_API
+    get() = P2M.apiOf(this)
+
+inline val <LAUNCHER : ModuleLauncher, MODULE_API : ModuleApi<LAUNCHER, *, *>> Class<out Module<MODULE_API>>.launcher: LAUNCHER
+    get() = P2M.launcherOf(this)
+
+inline val <SERVICE : ModuleService, MODULE_API : ModuleApi<*, SERVICE, *>> Class<out Module<MODULE_API>>.service: SERVICE
+    get() = P2M.serviceOf(this)
+
+inline val <EVENT : ModuleEvent, MODULE_API : ModuleApi<*, *, EVENT>> Class<out Module<MODULE_API>>.event: EVENT
+    get() = P2M.eventOf(this)
+
+inline val <MODULE_API : ModuleApi<*, *, *>> KClass<out Module<MODULE_API>>.api: MODULE_API
+    get() = P2M.apiOf(this.java)
+
+inline val <LAUNCHER : ModuleLauncher, MODULE_API : ModuleApi<LAUNCHER, *, *>> KClass<out Module<MODULE_API>>.launcher: LAUNCHER
+    get() = P2M.launcherOf(this.java)
+
+inline val <SERVICE : ModuleService, MODULE_API : ModuleApi<*, SERVICE, *>> KClass<out Module<MODULE_API>>.service: SERVICE
+    get() = P2M.serviceOf(this.java)
+
+inline val <EVENT : ModuleEvent, MODULE_API : ModuleApi<*, *, EVENT>> KClass<out Module<MODULE_API>>.event: EVENT
+    get() = P2M.eventOf(this.java)
